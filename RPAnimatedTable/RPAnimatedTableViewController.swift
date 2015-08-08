8a444597cc35926e1a63add0ce50276683ac03ea
naoyashiga <naoyashiga0@gmail.com>
2015-08-08 11:43:25 +0900

    set blur colors

--- RPAnimatedTable/RPAnimatedTableViewController.swift
+++ RPAnimatedTable/RPAnimatedTableViewController.swift
@@ -15,6 +15,21 @@ struct RPAnimatedTableReuseId {
 class RPAnimatedTableViewController: UITableViewController, RPAnimatedTableDelegate {
     var animation = RPTableAnimation()
     var animatedType = RPAnimatedTableType.Spring
+    
+    var colors: [String] = [
+        "0D47A1",
+        "1565C0",
+        "1976D2",
+        "1E88E5",
+        "2196F3",
+        "42A5F5",
+        "64B5F6",
+        "90CAF9",
+        "BBDEFB",
+        "E3F2FD"
+    ]
+    
+    var tableCount = 30
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,13 +54,16 @@ class RPAnimatedTableViewController: UITableViewController, RPAnimatedTableDeleg
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tableCount
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(RPAnimatedTableReuseId.cell, forIndexPath: indexPath) as! RPAnimatedTableViewCell
         
         cell.titleLabel.text = "title \(indexPath.row)"
+        
+        let colorIndex = indexPath.row % colors.count
+        cell.backgroundColor = UIColor.hexStr(colors[colorIndex], alpha: 1.0)
 
         return cell
     }
---
+++ RPAnimatedTable/UIColor+RPExtension.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+RPExtension.swift
+//  RPAnimatedTable
+//
+//  Created by naoyashiga on 2015/08/08.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class func hexStr (var hexStr : NSString, var alpha : CGFloat) -> UIColor {
+        hexStr = hexStr.stringByReplacingOccurrencesOfString("#", withString: "")
+        let scanner = NSScanner(string: hexStr as String)
+        var color: UInt32 = 0
+        if scanner.scanHexInt(&color) {
+            let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
+            let g = CGFloat((color & 0x00FF00) >> 8) / 255.0
+            let b = CGFloat(color & 0x0000FF) / 255.0
+            return UIColor(red:r,green:g,blue:b,alpha:alpha)
+        } else {
+            print("invalid hex string")
+            return UIColor.whiteColor();
+        }
+    }
+}
